64f3526df75ad45c2702faecb3d7dff25062209e
Adnan Quazi <86182471+AdnanQuazi@users.noreply.github.com>
2024-07-15 21:18:26 +0530

    Update index.js

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const mongoose = require("mongoose");
 const { ObjectId } = mongoose.Types;
 const port = process.env.PORT || 3000;
 const corsOptions = {
-  origin: ["http://localhost:5173", "http://192.168.1.39:5173" ,"https://zooptiq-pvgxu3jzk-adnan-quazis-projects.vercel.app"],
+  origin: ["http://localhost:5173", "http://192.168.1.39:5173" ,"https://zooptiq.vercel.app/"],
   methods: "GET, POST, PUT, DELETE, PATCH, HEAD",
   credentials: true,
 };
